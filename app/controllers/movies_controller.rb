f85d5a842e0516a83d678555b439528fcd6d5e8e
Gedeon Ntwali <ntwari28@gmail.com>
2017-01-18 16:29:37 +0200

    I added a new page called all_movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,11 @@ class MoviesController < ApplicationController
     render "movies.html.erb"
   end
 
+  def all_movies
+    @all_movies = Movie.all
+    render "all_movies.html.erb"
+  end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/movies", to: 'movies#movies'
+  get "/all_movies", to: 'movies#all_movies'
 end
